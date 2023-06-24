a5ac9da82d5c1b714107343fe3d69e4a413c6966
shivamanibrt <shivamanibrt03350@gmail.com>
2023-06-24 22:57:51 +1000

    s

--- src/Components/ReusableComponent/CardComponent.js
+++ src/Components/ReusableComponent/CardComponent.js
@@ -6,7 +6,7 @@ export const CardComponent = ({ carModal, carImg }) => {
         <Card className="text-center border-1" style={{ height: '150px' }}>
             <Card.Img src={carImg} className="card-img-filter" style={{ filter: 'brightness(50%)', opacity: '1' }} />
             <Card.ImgOverlay className="d-flex align-items-center justify-content-center">
-                <Card.Text className="text-light"><h2>{carModal}</h2></Card.Text>
+                <Card.Text className="text-light"><h1>{carModal}</h1></Card.Text>
             </Card.ImgOverlay>
         </Card>
     );
