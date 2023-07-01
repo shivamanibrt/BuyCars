0e49cd4193de1dac950d1ed4fd67e3a27caaed60
shivamanibrt <shivamanibrt03350@gmail.com>
2023-07-01 19:02:34 +1000

    Your commit message

--- src/Admin/AdminLayout/AdminLayout.js
+++ src/Admin/AdminLayout/AdminLayout.js
@@ -44,6 +44,7 @@ export const AdminLayout = ({ children }) => {
                                 </li>
                             </>
                         )}
+
                     </ul>
                 </div>
 
