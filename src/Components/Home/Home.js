e6de0ccfc9bdf73a0e4abd19bb317b3c7dfa97f1
shivamanibrt <shivamanibrt03350@gmail.com>
2023-07-20 09:36:01 +1000

    before stripe

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { HeroSection } from '../HeroSection/HeroSection'
+import { CarTypes } from '../CarTypes/CarTypes'
+import { PopularProducts } from '../PopularProducts/PopularProducts'
+import { NewsLetter } from '../NewsLetter/NewsLetter'
+
+export const Home = () => {
+    return (
+        <div>
+            <HeroSection />
+            <CarTypes />
+            <PopularProducts />
+            <NewsLetter />
+        </div>
+    )
+}
