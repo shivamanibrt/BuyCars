53bcb42aaeefcee803d913cd08432aeec6403191
shivamanibrt <shivamanibrt03350@gmail.com>
2023-07-20 11:00:59 +1000

    “message”

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
